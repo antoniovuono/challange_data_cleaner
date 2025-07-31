a7a782b9a89831be2e1e3ef46cadfe64feec4307
Antonio Vuono <antoniosvuono@icloud.com>
2025-07-31 17:40:43 -0300

    feature: df coluns

--- src/clean_data.py
+++ src/clean_data.py
@@ -16,7 +16,35 @@ print(df.info())
 
 # 5. Verifique a quantidade de duplicatas na nossa base de dados
 print("Quantidade de itens duplicados:", df.duplicated().sum())
+# remvoer duplicatas
+df = df.drop_duplicates()
+print("Quantidade de itens duplicados:", df.duplicated().sum())
 
 # 6. Gere estatísticas básicas
 print("\n📊 Estatísticas descritivas:")
 print(df.describe(include='all'))
+
+# Verificar a quantidade de valores nulos por coluna
+print("Quantidade de valores nulos")
+print(df.isnull().sum())
+
+# Forçar conversão das colunas numéricas:
+df["quantidade"] = pd.to_numeric(df["quantidade"], errors="coerce")
+df["preco_unitario"] = pd.to_numeric(df["preco_unitario"], errors="coerce")
+
+# Remover linhas com NaN ou preço <= 0:
+df = df.dropna(subset=["produto", "quantidade", "preco_unitario", "vendedor"])
+df = df[df["preco_unitario"] > 0]
+
+print("\n✅ Dataset após limpeza:")
+print(df.info())
+
+# Criar coluna valor total (qunatidade x preço)
+df['valor_total'] = df['quantidade'] * df['preco_unitario']
+total_vendas = df['valor_total'].sum()
+print("O valor total de vendas:", total_vendas)
+
+# Produto mais vendido por quantidade:
+produto_mais_vendido = df.groupby("produto")["quantidade"].sum().sort_values(ascending=False)
+print("\n📦 Produto mais vendido (por quantidade):")
+print(produto_mais_vendido.head(1))

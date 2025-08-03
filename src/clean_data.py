56de6c40ab9d0acbc1e8e8589d3ba436ffd395c6
Antonio Vuono <antoniosvuono@icloud.com>
2025-08-03 14:28:49 -0300

    refactor: isolate responsabilities in functions

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,48 +1,58 @@
+from itertools import groupby
 import pandas as pd
 
 # 🔹 Etapa 1: Leitura do arquivo
 
-data_path = "data/vendas_ficticias.csv"
-df = pd.read_csv(data_path)
+def load_data(path):
+    print("Lê o arquivo CSV e retorna um dataframe")
+    df = pd.read_csv(path)
+    return df
 
-print(df)
 
 # 🔹 Etapa 2: Diagnóstico e limpeza
 
-print("\n🔍 Diagnóstico inicial:")
-print(df.info())
-print("\nValores nulos por coluna:")
-print(df.isnull().sum())
-print("\nduplicados:", df.duplicated().sum())
-
 # 2.1 Limpeza
-df = df.drop_duplicates()
-df["quantidade"] = pd.to_numeric(df['quantidade'], errors="coerce")
-df["preco_unitario"] = pd.to_numeric(df['preco_unitario'], errors="coerce")
-df = df.dropna(subset=["produto", "quantidade", "preco_unitario","data", "vendedor"])
-df = df[df["preco_unitario"] > 0]
+def clean_data(df):
+    df = df.drop_duplicates()
+    df["quantidade"] = pd.to_numeric(df['quantidade'], errors="coerce")
+    df["preco_unitario"] = pd.to_numeric(df['preco_unitario'], errors="coerce")
+    df = df.dropna(subset=["produto", "quantidade", "preco_unitario","data", "vendedor"])
+    df = df[df["preco_unitario"] > 0]
+    df["valor_total"] = df["quantidade"] * df["preco_unitario"]
+    return df
 
-# 🔹 Etapa 3: Análise e estatísticas
+# 🔹 Etapa 3: Análise e estatísticas: Relatório
 
-# Descubra o valor total em vendas:
-df["valor_total"] = df["quantidade"] * df["preco_unitario"]
-print(f"\n Valor total de vendas: {df["valor_total"].sum():,.2f}")
+def generate_report(df):
+        """Imprime estatísticas úteis sobre os dados"""
+        total = df["valor_total"].sum()
+        print(f"\n💰 Valor total de vendas: R$ {total:,.2f}")
+
+        print("\n Produto mais vendido:")
+        # Agrupa por produtos, pega a coluna quantidade e traz a soma, ordena do maior para o menor e pega o primeiro.
+        print(df.groupby("produto")["quantidade"].sum().sort_values(ascending=False).head(1))
+
+        print("\n Vendedor que mais vendeu: ")
+        best_seller = df.groupby("vendedor")["valor_total"].sum().sort_values(ascending=False).head(1)
+        print(best_seller)
+
+        print("\n Média de preço por produto vendido:")
+        # tirar dúvida do mean:
+        average_price = df.groupby('produto')['preco_unitario'].mean().sort_values(ascending=False)
+        print(average_price)
+
+def export_data_cleaned(df, path):
+    df.to_csv(path, index=False)
+    print(f"\n📁 Arquivo exportado para: {path}")
 
 
-print("\n Produto mais vendido:")
-# Agrupa por produtos, pega a coluna quantidade e traz a soma, ordena do maior para o menor e pega o primeiro.
-print(df.groupby("produto")["quantidade"].sum().sort_values(ascending=False).head(1))
+# Pipeline principal:
 
-print("\n Vendedor que mais vendeu: ")
-best_seller = df.groupby("vendedor")["valor_total"].sum().sort_values(ascending=False).head(1)
-print(best_seller)
+if __name__ == "__main__":
+    path = "data/vendas_ficticias.csv"
+    export_path = "data/vendas_limpo.csv"
 
-
-print("\n Média de preço de um produto: ")
-average_price = df.groupby('produto')['preco_unitario'].mean().sort_values(ascending=False)
-print(average_price)
-
-# 🔹 Etapa 4: Exportação opcional
-
-df.to_csv("data/vendas_limpo.csv", index=False)
-print("Dataframe limpo exportado p/ csv!")
+    df = load_data(path)
+    df_cleaned = clean_data(df)
+    generate_report(df_cleaned)
+    export_data_cleaned(df_cleaned, export_path)
